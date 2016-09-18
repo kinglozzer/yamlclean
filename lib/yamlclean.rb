1afc090ceb07b79a93300888c6c537dd5695743e
Sam Minnee <sam@silverstripe.com>
2016-09-18 13:40:44 +1200

    FIX: More output clean-ups
    Remove extraneous trailing whitespace and --- separator from output.

--- lib/yamlclean.rb
+++ lib/yamlclean.rb
@@ -9,6 +9,15 @@ module YamlClean
   	yamlString = IO.read(filename)
   	dirty = YAML.load(yamlString)
 	  cleaned = dirty.ya2yaml(:syck_compatible => true)
+
+    # Remove trailing whitespace
+    cleaned = cleaned.gsub(/ +\n/, "\n")
+
+    # Remove unnecessary --- header
+    if(cleaned.scan(/^---$/).size == 1) then
+      cleaned = cleaned.gsub(/^---$\n?/, "")
+    end
+
 	  return cleaned
   end
 end
